006cf21ca162660cf8df2d59b3955832d6c03fc9
alexandrosok <alexandrosok@hotmail.com>
2020-10-13 17:45:53 +0100

    initializing

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const indexRouter = require('./routes/index');
+const app = express();
+const port = 4007;
+require('dotenv').config();
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', indexRouter);
+
+app.listen(port, () => {
+    console.log(`No sign of life did flicker In floods of tears she cried `, `http://localhost:${port}`)
+});
+module.exports = app;
